54b2fbd1ab2f6011caf5fe18b57cca173281e3c1
tang55555song <2392950167@qq.com>
2016-06-13 19:38:36 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/player/MainActivity.java
+++ app/src/main/java/com/example/administrator/player/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //你好
+        //你好吗？
     }
 }
